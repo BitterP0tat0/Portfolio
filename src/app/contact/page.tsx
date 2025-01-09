95bcf2708fd7d25e9338b2cf23ef83b34371e4a9
BitterP0tat0 <98937878+BitterP0tat0@users.noreply.github.com>
2025-01-09 02:15:48 +0100

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,26 @@
+"use client";
 import Nav from "@/Components/Nav";
-import Contact from "@/Components/Contact";
 import Cursor from "@/Components/Cursor";
-
-export default function Page() {
+import Info from "@/Components/Info";
+import { Vortex } from "@/Components/ui/vortex";
+export default function myself() {
   return (
-    <div className="bg-white dark:bg-gray-800 cursor-none">
+    <div className="my-auto mx-auto cursor-none cursor: none">
       <Nav></Nav>
-
-      <Contact />
+      <Info></Info>
+      <div className="w-full h-[200] mx-auto overflow-hidden">
+        <Vortex
+          backgroundColor="black"
+          className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
+        >
+          <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
+            This is me
+          </h2>
+          <p className="text-white text-xl md:text-2xl max-w-xl mt-6 text-center">
+            The one who always improve himself
+          </p>
+        </Vortex>
+      </div>
       <Cursor></Cursor>
     </div>
   );
