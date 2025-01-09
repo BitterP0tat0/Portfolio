39e5b40783453dbbc19f25a8dd59c17042ee0f96
BitterP0tat0 <98937878+BitterP0tat0@users.noreply.github.com>
2025-01-09 15:02:56 +0100

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,9 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off", // 禁用 no-explicit-any 规则
-    },
+    rules: {}, 
   },
 ];
 
