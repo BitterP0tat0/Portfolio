acee6a1fc90d98e7601554e03c3a9390f8593969
BitterP0tat0 <98937878+BitterP0tat0@users.noreply.github.com>
2025-01-09 15:09:48 +0100

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,8 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    rules: {}, 
+    rules: { 'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',}, 
   },
 ];
 
