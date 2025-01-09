b84c95387e3171adfbd31906017e64f3f1e79e0e
BitterP0tat0 <a251080262@gmail.com>
2025-01-09 20:15:46 +0100

    improve the layout

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,7 @@ import Cursor from "@/Components/Cursor";
 import Contact from "@/Components/Contact";
 export default function myself() {
   return (
-    <div className="my-auto mx-auto cursor-none cursor: none">
+    <div className="my-auto mx-auto cursor-none">
       <Nav></Nav>
       <Contact></Contact>
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import Nav from "@/Components/Nav";
 import About from "@/Components/About";
 import Cursor from "@/Components/Cursor";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Page() {
   return (
-    <div className="bg-white dark:bg-gray-800 cursor-none">
+   
+    <div className="bg-white dark:bg-gray-800 cursor-none"> 
       <Nav></Nav>
 
       <About></About>
       <Cursor></Cursor>
-      
+      <Analytics></Analytics>
     </div>
   );
 }
